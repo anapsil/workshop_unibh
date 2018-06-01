0cefe80b0f530c89915800269d2ae0dbd6d4097d
apsilva <apsilva@thoughtworks.com>
2018-06-01 18:56:49 -0300

    Implemented take and share picture methods

--- app/src/main/java/com/example/apsilva/workshopunibh/MainActivity.kt
+++ app/src/main/java/com/example/apsilva/workshopunibh/MainActivity.kt
@@ -1,24 +1,42 @@
 package com.example.apsilva.workshopunibh
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.content.pm.PackageManager.PERMISSION_GRANTED
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.provider.MediaStore
+import android.support.v4.view.MenuItemCompat
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.ShareActionProvider
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
+const val CAMERA_REQUEST = 1888
+const val CAMERA_PERMISSION_CODE = 100
+
 class MainActivity : AppCompatActivity() {
+    var shareActionProvider: ShareActionProvider? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        takePicture.setOnClickListener { _ -> takePicture() }
+        takePicture.setOnClickListener { _ -> checkCameraPermission() }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.menu_share, menu)
-        return super.onCreateOptionsMenu(menu)
+        val menuItem = menu?.findItem(R.id.action_share)
+
+        shareActionProvider = MenuItemCompat.getActionProvider(menuItem) as ShareActionProvider
+
+        shareActionProvider?.setShareIntent(getShareIntent())
+        return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
@@ -28,11 +46,49 @@ class MainActivity : AppCompatActivity() {
         return super.onOptionsItemSelected(item)
     }
 
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == CAMERA_PERMISSION_CODE) {
+            if (grantResults[0] == PERMISSION_GRANTED) {
+                takePicture()
+            } else {
+                showPermissionDeniedMessage()
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
+            val photo = data?.extras?.get("data") as Bitmap
+            picture.setImageBitmap(photo)
+        }
+    }
+
+    private fun checkCameraPermission() {
+        if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(arrayOf(Manifest.permission.CAMERA), CAMERA_PERMISSION_CODE)
+        } else {
+            takePicture()
+        }
+    }
+
     private fun takePicture() {
-        Toast.makeText(this, R.string.take_picture, Toast.LENGTH_SHORT).show()
+        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(cameraIntent, CAMERA_REQUEST)
     }
 
     private fun sharePicture() {
         Toast.makeText(this, R.string.share_picture, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun getShareIntent(): Intent {
+        val shareIntent = Intent(Intent.ACTION_SEND)
+        shareIntent.type = "image/*"
+//        myShareIntent.putExtra(Intent.EXTRA_STREAM, myImageUri)
+        return shareIntent
+    }
+
+    private fun showPermissionDeniedMessage() {
+
     }
 }
